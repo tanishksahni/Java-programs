a55d5049096eb4c1348317ad0e118660f97c94e1
tanishk_sahni <tanishk.sahni2002@gmail.com>
2022-04-14 07:35:27 +0530

    Starting event java projects

---
+++ unino.java
@@ -0,0 +1,48 @@
+import java.util.*;
+class unino
+{
+    public static void main(String[] args)
+    {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the size of array and array elements");
+        int n=sc.nextInt();
+        int arr[] = new int [n];
+        for(int i=0; i<n; i++)
+        {
+            arr[i]=sc.nextInt();
+        }
+        System.out.println("Enter the number of rotations");
+        int r=sc.nextInt();
+        int num=0, diff=0;
+        int fpos=0;
+        num=arr[0];
+        int uno=num;
+        while (r>=0)
+        {
+            int i=0;
+            if (diff==0){
+
+                while (i<n)
+                {
+                   uno=arr[i];
+                   i+=num;    
+                   fpos=i;
+                }
+                diff=num-(n-fpos);
+            }
+            else{
+                num=diff;
+                while (i<n)
+                {
+                    uno=arr[i];
+                    i+=num;    
+                    fpos=i;
+                }
+                diff=num-(n-fpos);
+               
+            }    
+            r-=1;
+        }
+        System.out.println("UNIQUE NUMBER: "+ uno);
+    }
+}
